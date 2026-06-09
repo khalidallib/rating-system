a90515b57cee2d0a562cc4cf9007501a66a0fc2a
Khalid Alli-B <khalidallibalogun08@gmail.com>
2026-06-08 22:21:24 -0500

    first app.js layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import Star from "./components/Star";
+import "./css/index.css";
+
 function App() {
   return (
-    <></>
+    <main className="app">
+      <h1>Star Rating System</h1>
+      <Star />
+      <button className="dialog-button">
+        Open Dialog
+      </button>
+    </main>
   );
 }
 
