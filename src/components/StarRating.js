6dddff3c1919d86a40fcde046054069b0fce904f
Khalid Alli-B <khalidallibalogun08@gmail.com>
2026-06-08 22:55:29 -0500

    added rating words for each star level

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import Star from "./Star";
+
 function StarRating() {
+  const [rating, setRating] = useState(0);
+
+  const ratingWords = [ "", "Poor","Fair", "Good", "Very Good", "Excellent"];
+
   return (
     <div>
       {[1, 2, 3, 4, 5].map((starNumber) => (
-        <Star key={starNumber} />
+        <Star
+          key={starNumber}
+          selected={starNumber <= rating}
+          onClick={() => setRating(starNumber)}
+        />
       ))}
-      <h2>Select a rating</h2>
+
+      <h2>{ratingWords[rating]}</h2>
     </div>
   );
 }
